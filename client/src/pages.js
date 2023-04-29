be84b0757a06ff6718cfe99cb9f33f9bacd6fb79
flyseddy <sedrick100thomas@gmail.com>
2023-04-29 11:46:56 -0400

    Added Login functionality

--- client/src/pages.js
+++ client/src/pages.js
@@ -3,8 +3,26 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 /** importing our pages */
 import { Concerts }  from './concerts';
 import { ConcertDetail } from './concertDetail';
+import Login from './components/Login/Login';
+import useToken from './useToken';
+
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
 
 export function Pages() {
+  const { token, setToken } = useToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <BrowserRouter>
       <Routes>
